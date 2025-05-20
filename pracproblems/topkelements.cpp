e6667dea5b7ce4e5d3771e7b553e32aebfad4995
c-kappel <kcapcode@gmail.com>
2025-05-19 21:16:07 -0400

    practice problem with unordered_map, vector.

--- pracproblems/topkelements.cpp
+++ pracproblems/topkelements.cpp
@@ -2,6 +2,28 @@
 #include <vector>
 #include <unordered_map>
 
+std::vector<int> topKFrequent(std::vector<int>& nums, int k){
+        std::unordered_map<int, int> map;
+        for(int uniqueNum: nums){
+            map[uniqueNum] += 1;
+        }
+
+        std::vector<int> out;
+        while(k > 0){
+            int largest = 0;
+            int largestNum = 0;
+            for(int num : nums){
+                if(map[num] > largest){
+                    largest = map[num];
+                    largestNum = num;
+                }
+            }
+            out.push_back(largestNum);
+            map[largestNum] = -1;
+            k--;
+        }
+        return out;
+    }
 
 int main(){
 
